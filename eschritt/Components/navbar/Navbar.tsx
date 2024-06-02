036965b5a4a1ef15e3e788f2a963c488cda8ee1d
Mostafa <mostafa.hafez.de@gmail.com>
2024-06-02 22:28:46 +0200

    navbar enhanced

--- eschritt/Components/navbar/Navbar.tsx
+++ eschritt/Components/navbar/Navbar.tsx
@@ -1,24 +1,37 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+
+import Link from 'next/link';
+import React, { useState } from 'react';
 import { PiWebhooksLogoFill } from "react-icons/pi";
-import style from './navbar.module.css'
+import { AiOutlineMenu } from "react-icons/ai";
+import { IoMdClose } from "react-icons/io";
+
+import style from './navbar.module.css';
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
+
     return (
         <div>
             <nav className={style.navbar}>
-                <div className={style.navLogo}>
-                    MOSMO <PiWebhooksLogoFill/> YAS
+                <div>
+                    <Link className={style.navLogo} href="/">
+                        MOSMO<PiWebhooksLogoFill /> YAS
+                    </Link>
                 </div>
-                <div className={style.navLinks}>
-                  
-                    <Link  className={style.navLink} href={"./admin"}>  Admin</Link>
-                    <Link  className={style.navLink}  href={"./articles"}> Articles</Link>
-                    <Link  className={style.navLink} href={"./about"}>  About Me</Link>
+                <div className={style.menu} onClick={() => setToggle(prev => !prev)}>
+                    {toggle ? <IoMdClose /> : <AiOutlineMenu />}
+                </div>
+                <div className={`${style.navLinksWrapper} ${toggle ? style.open : ''}`}>
+                    <div className={style.navLinks}>
+                        <Link onClick={()=>setToggle(false)}  className={style.navLink} href="/admin">Admin</Link>
+                        <Link  onClick={()=>setToggle(false)}className={style.navLink} href="/articles">Articles</Link>
+                        <Link onClick={()=>setToggle(false)} className={style.navLink} href="/about">About Me</Link>
+                    </div>
                 </div>
             </nav>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
